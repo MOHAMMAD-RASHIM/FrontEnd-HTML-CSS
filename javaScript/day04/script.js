523ecafc1a12d7b60191493b3d210492ec9aaed9
MOHAMMAD-RASHIM <mohammadrashimkallaje@gmail.com>
2026-09-11 10:43:19 +0530

    add mcq related questions

--- javaScript/day04/script.js
+++ javaScript/day04/script.js
@@ -1,3 +1,28 @@
-let isEligible = true;
+// // let isEligible = true;
 
-console.log(typeof(isEligible));
+// // console.log(typeof(isEligible));
+
+// console.log(1 + true); // 2
+
+// console.log(true + true); // 2
+
+// console.log(true + false); // 1 + 0 = 1
+
+// console.log(false * 3.1456); // 0 * 3.1456 = 0
+
+// console.log(Boolean("Day 04")); // truthy value
+// console.log(Boolean(" ")); // truthy value
+// console.log(Boolean("")); // falsy value
+
+// let a;
+// console.log(a); // undefined
+// console.log(typeof a); // undefined
+
+// let a  = null;
+// console.log(a);
+// console.log(typeof a);
+
+// console.log(undefined + 20); //NaN
+// console.log(NaN + 20); // NaN
+// console.log(null + 20); //20
+
